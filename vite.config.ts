cbb3f920894530b694fe3e8ef6fd89aa282328d3
btd <cdrom205@gmail.com>
2026-04-28 09:54:43 +0100

    Ignore test report dir & files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,5 +10,8 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom', // required for JS component testing
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    setupFiles: './src/test/setup.ts',
+    reporters: ['default', 'html'],
+    outputFile: './src/test/report/index.html',
   },
 });
