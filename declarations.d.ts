1a4f0a5ef7234f723773c16f234e1aed45430145
btd <cdrom205@gmail.com>
2026-05-03 13:17:58 +0100

    Fix side-effect import error for global css

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,6 +3,9 @@ declare module '*.module.css' {
   export default classes;
 }
 
+// NO need to include 'export default src' because side-effect imports don't use it
+declare module '*.css';
+
 declare module '*.mp3' {
   const src: string;
   export default src;
